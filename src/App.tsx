591298a9ddbfd9dae7d6699d6b2f04322a5ecdb0
Riki Itokazu <itokazuriki@gmail.com>
2024-09-24 15:46:51 -0400

    feat: initialized routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element= {<div>Hello</div>} /> 
+        <Route path = "/redux" element = {<div>Redux</div>} />
+        <Route path = "/recoil" element = {<div>Recoil</div>} />
+        <Route path = "/jotai" element = {<div>Jotai</div>} />
+        <Route path = "/zustand" element = {<div>Zustand</div>} />
+        <Route path = "/contextapi" element = {<div>Context API</div>} />
+      </Routes>
+    </Router>
   )
 }
 
