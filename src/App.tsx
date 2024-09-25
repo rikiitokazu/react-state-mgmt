97709cd78b550a2647507dfc07e26bb088b7291e
Riki Itokazu <itokazuriki@gmail.com>
2024-09-25 00:59:29 -0400

    feat: redux started

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-
+import LandingPage from './LandingPage'
 function App() {
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element= {<div>Hello</div>} /> 
+        <Route path="/" element= {<LandingPage />} /> 
         <Route path = "/redux" element = {<div>Redux</div>} />
         <Route path = "/recoil" element = {<div>Recoil</div>} />
         <Route path = "/jotai" element = {<div>Jotai</div>} />
